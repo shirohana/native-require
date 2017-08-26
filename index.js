e8bf4d6d8c38a60cdf12cf48b21fb97ffadafdff
Hana Shiro <shirohana0608@gmail.com>
2017-08-26 10:29:54 +0800

    [Main] Update `index` with full ES5 syntax

--- index.js
+++ index.js
@@ -1,41 +1,39 @@
-const Module = require('module')
-const path = require('path')
+const Module = require('module');
+const path = require('path');
 
 // To solve relative requirings and resolvings for `nrequire.from`
-const NON_EXISTS_FILENAME = '__native_require__'
+const NON_EXISTS_FILENAME = '__native_require__';
 
-function NativeRequire (options) {
-  options = options || {};
-  const basedir = (typeof options.basedir === 'string' ? path.resolve(options.basedir) : process.cwd())
-
-  const nativeModule = {
+function nativeRequire (_options) {
+  const options = _options || {};
+  const basedir = (typeof options.basedir === 'string' ? path.resolve(options.basedir) : process.cwd());
+  const nmodule = {
     id: '<repl>',
     exports: {},
-    parent: undefined,
+    parent: module.parent,
     filename: path.join(basedir, NON_EXISTS_FILENAME),
     loaded: false,
     children: [],
     paths: Module._nodeModulePaths(basedir)
-  }
+  };
 
-  const nativeRequire = Module.prototype.require.bind(nativeModule)
-  nativeRequire.resolve = function (request) {
-    return Module._resolveFilename(request, nativeModule)
-  }
-  nativeRequire.main = process.mainModule
-  nativeRequire.extensions = Module._extensions
-  nativeRequire.cache = Module._cache
+  const nrequire = Object.assign(Module.prototype.require.bind(nmodule), {
+    main: process.mainModule,
+    extensions: Module._extensions,
+    cache: Module._cache,
+    resolve: function resolve (request) {
+      return Module._resolveFilename(request, nmodule);
+    },
+    from: function from (_basedir) {
+      return nativeRequire({
+        basedir: _basedir
+      });
+    }
+  });
 
-  // Support custom basedir
-  nativeRequire.from = function (basedir) {
-    return NativeRequire({
-      basedir: basedir
-    })
-  }
-
-  return nativeRequire
+  return nrequire;
 }
 
-const instance = NativeRequire()
+const instance = nativeRequire();
 
-module.exports = instance
+module.exports = instance;
